14407545448e19b88752125b586d2ffa53231bfd
Gökhan Türk <ensar.gokhan@hotmail.com>
2026-02-08 17:29:14 +0300

    Changing URLs to test

--- bypasseng.cmd
+++ bypasseng.cmd
@@ -1,2 +1,2 @@
-curl -L -o C:\Windows\Panther\unattendedeng.xml https://raw.githubusercontent.com/GokhanTurk/skipwinstallation/refs/heads/main/unattendedeng.xml
+curl -L -o C:\Windows\Panther\unattendedeng.xml https://raw.githubusercontent.com/GokhanTurk/skipwinstallation/refs/heads/feature/test/unattendedeng.xml
 %WINDIR%\System32\Sysprep\Sysprep.exe /generalize /oobe /unattend:C:\Windows\Panther\unattendedeng.xml /reboot
